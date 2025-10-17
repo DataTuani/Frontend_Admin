794090a83f015367421c5caab21cd04d8602eb92
isCris-Q05 <cristopherquintana2725@gmail.com>
2025-10-17 07:18:25 -0600

    att netlify 5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,7 +10,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    minify: false,
   },
   base: './'
 })
+
